d012583d170fb774efc0c266f372b1ffbf8f3670
laustidio <breno_laustidio@hotmail.com>
2025-03-12 21:43:54 +0100

    login components

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,29 +1,27 @@
 import React from "react";
 
 interface InputProps {
-    type: string;
-    placeholder: string;
-    value: string;
-    onChange: React.ChangeEventHandler<HTMLInputElement>;
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-const Input: React.FC<InputProps> = ({ type, placeholder, value, onChange }) => {
-    return <input
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className={`
-         w-full p-3
-        border-gray-300 
-        rounded-md 
-        focus:outline-none 
-        focus:ring-2 
-        focus:ring-blue-500
-        bg-white
-        text-black
-        `}
-    />;
-}
+const Input: React.FC<InputProps> = ({
+  type,
+  placeholder,
+  value,
+  onChange,
+}) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className={`w-full p-3 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-black`}
+    />
+  );
+};
 
 export default Input;
