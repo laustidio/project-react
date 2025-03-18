94ac7e60bae720307913bdee037ee81a0200f8da
laustidio <breno_laustidio@hotmail.com>
2025-03-18 22:28:26 +0100

    Minor correction on FormSignup

--- src/components/features/forms/form-signup/FormSignup.tsx
+++ src/components/features/forms/form-signup/FormSignup.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import * as Yup from 'yup';
-import Input from '../../../ui/input/Input';
-import Button from '../../../ui/button/Button';
-import Checkbox from '../../../ui/checkbox/Checkbox';
-import { FormSignupStyles } from './FormSignupStyles';
-import { FormProvider, SubmitHandler } from 'react-hook-form';
-import { useFormValidation } from '../../../../hooks/useFormValidation';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import * as Yup from "yup";
+import Input from "../../../ui/input/Input";
+import Button from "../../../ui/button/Button";
+import Checkbox from "../../../ui/checkbox/Checkbox";
+import { FormSignupStyles } from "./FormSignupStyles";
+import { FormProvider, SubmitHandler } from "react-hook-form";
+import { useFormValidation } from "../../../../hooks/useFormValidation";
+import { useNavigate } from "react-router-dom";
 
 const schema = Yup.object().shape({
-  username: Yup.string().min(3, "Minimum 3 characters.").required('Username is required.'),
+  username: Yup.string().min(3, "Minimum 3 characters.").required("Username is required."),
   password: Yup.string()
-    .min(6, 'Minimum 6 characters.')
-    .required('Password is required.'),
+    .min(6, "Minimum 6 characters.")
+    .required("Password is required."),
   confirmPwd: Yup.string()
-    .oneOf([Yup.ref('password')], 'The passwords do not match.')
-    .required('Password confirmation is mandatory.'),
+    .oneOf([Yup.ref("password")], "The passwords do not match.")
+    .required("Password confirmation is mandatory."),
   email: Yup.string()
-    .email('Email is not valid.')
-    .required('Email is required.'),
-  isAcceptTherms: Yup.boolean().oneOf([true], 'You must accept the therms.'),
+    .email("Email is not valid.")
+    .required("Email is required."),
+  isAcceptTherms: Yup.boolean().oneOf([true], "You must accept the therms."),
 });
 
 const FormSignup: React.FC = () => {
@@ -29,7 +29,7 @@ const FormSignup: React.FC = () => {
   const { reset } = methods;
 
   const onSubmit: SubmitHandler<Yup.InferType<typeof schema>> = (data) => {
-    console.log('Data', data);
+    console.log("Data", data);
     reset();
   };
 
@@ -45,32 +45,32 @@ const FormSignup: React.FC = () => {
 
         <h2 className={FormSignupStyles.h2}>Black Forge</h2>
 
-        <Input type='text' placeholder='Username' name='username' />
-        <Input type='password' placeholder='Password' name='password' />
-        <Input type='password' placeholder='Confirm password' name='confirmPwd' />
-        <Input type='email' placeholder='Email' name='email' />
+        <Input type="text" placeholder="Username" name="username" />
+        <Input type="password" placeholder="Password" name="password" />
+        <Input type="password" placeholder="Confirm password" name="confirmPwd" />
+        <Input type="email" placeholder="Email" name="email" />
 
         <Checkbox
           label={
             <>
-              I confirm that I have read, consent and agree to Black Forge's{" "}
+              I confirm that I have read, consent and agree to Black Forge"s{""}
               <a href="" className="text-cyan-400 hover:underline">
                 Terms of Use
-              </a>{" "}
-              and{" "}
+              </a>{""}
+              and{""}
               <a href="" className="text-cyan-400 hover:underline">
                 Privacy Policy
               </a>.
             </>
           }
-          name='isAcceptTherms'
+          name="isAcceptTherms"
         />
 
-        <div className='flex w-full justify-between items-center'>
-          <Button onClick={handleCancel} variant='secondary'>
+        <div className="flex w-full justify-between items-center">
+          <Button onClick={handleCancel} variant="secondary">
             Cancel
           </Button>
-          <Button type='submit' variant='primary'>
+          <Button type="submit" variant="primary">
             Signup
           </Button>
         </div>
